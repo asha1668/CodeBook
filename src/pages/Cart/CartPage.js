67c85087291b2e2a3437a1fc5ff378625518a411
Asha Badavagol <ashabadavagol1668@gmail.com>
2023-10-19 00:34:42 +0530

    add file

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,7 +1,7 @@
 import { CartEmpty } from "./components/CartEmpty";
 import { CartList } from "./components/CartList";
-import { useCart } from "../../context";
 import { useTitle } from "../../hooks/useTitle";
+import { useCart } from "../../context";
 
 export const CartPage = () => {
 const { cartList } = useCart();
